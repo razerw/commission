4207e3aa8eb9a62b6b19cb07a4687cccbb75c996
test <razerw@yandex.ru>
2025-05-02 13:55:11 +0300

    “”

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -18,7 +18,7 @@ fun main() {
 
 fun moneyOrder(cardType: String, transfer: Int, maxLimit: Int, transactionLimit: Int, transferAmountInMonth: Int): Int {
     return when (cardType) {
-        "Мир" -> mirLimit(transfer, maxLimit, transactionLimit, transferAmountInMonth)
+        "Ми" -> mirLimit(transfer, maxLimit, transactionLimit, transferAmountInMonth)
         "Mastercard" -> mastercardLimitAndCommission(transfer, maxLimit, transactionLimit, transferAmountInMonth)
         "Visa" -> visaLimitCommission(transfer, maxLimit, transactionLimit, transferAmountInMonth)
         else ->
